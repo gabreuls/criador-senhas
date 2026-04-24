4c5d346268d23a1ecaccda0c6dfc795f1f7c5f11
Gabriel <antunes.gsilva@gmail.com>
2026-04-24 09:14:03 -0300

    Integrando aplicação ao discord

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Sim, eu sei que poderia utilizar strings que obteria o mesmo resultado, optei por listas por causa de clareza de pensamento! 
 
-import random
+import random, time
 
 letras_minusculas = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 letras_maiusculas = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
